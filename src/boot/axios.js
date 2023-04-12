20e7867bdba4f6f6469fd12c37ff65050f9d55f4
Juli Soeprapto <juli.iot@ubl.ac.id>
2023-04-12 21:43:38 +0700

    front capil

---
+++ src/boot/axios.js
@@ -0,0 +1,19 @@
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
+
+const baseURL = 'http://localhost:5088/'
+
+const api = axios.create({ baseURL: baseURL })
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$axios = api
+})
+
+export { api }
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', name: 'dashboard', component: () => import('pages/IndexPage.vue') },
+      { path: 'export', name: 'export', component: () => import('pages/ExportPage.vue') },
+      { path: 'import', name: 'import', component: () => import('pages/ImportPage.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
